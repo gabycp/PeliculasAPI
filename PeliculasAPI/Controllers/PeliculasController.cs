0ebcc2e89c580101a0c95331974d39a97ec72275
Gabriela Alejandra Cornejo <gaby_cor93@hotmail.com>
2024-07-26 04:38:02 -0600

    Se crea y se agrega la Entidad de Peliculas y se comeienza agregar sus endpoints.

---
+++ PeliculasAPI/Controllers/PeliculasController.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PeliculasAPI.DTOs;
+using PeliculasAPI.Servicios;
+
+namespace PeliculasAPI.Controllers
+{
+    [ApiController]
+    [Route("api/peliculas")]
+    public class PeliculasController: ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
+        private readonly IAlmacenadorArchivos almacenadorArchivos;
+
+        public PeliculasController(ApplicationDbContext context,
+                                    IMapper mapper,
+                                    IAlmacenadorArchivos almacenadorArchivos)
+        {
+            this.context = context;
+            this.mapper = mapper;
+            this.almacenadorArchivos = almacenadorArchivos;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<PeliculaDTO>>> Get() 
+        {
+            var peliculas = await context.Peliculas.ToListAsync();
+
+            return mapper.Map<List<PeliculaDTO>>(peliculas);
+        }
+
+        [HttpGet("{id}", Name = "obtenerPelicula")]
+        public async Task<ActionResult<PeliculaDTO>> Get(int id) 
+        {
+            var pelicula = await context.Peliculas.FirstOrDefaultAsync(x => x.Id == id);
+
+            if(pelicula == null) return NotFound();
+
+            return mapper.Map<PeliculaDTO>(pelicula);
+        }
+
+    }
+}
